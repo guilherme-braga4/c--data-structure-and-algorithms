bfef285a9c2e1931e665690e0729f1c9e0370857
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2024-03-05 08:15:14 -0300

    math operators

--- first/main.cpp
+++ first/main.cpp
@@ -5,6 +5,11 @@ int main()
 {
   int number;
   number = 50;
+  int bigNumber = 3000;
+  cout << bigNumber << endl;
+  int sum = number + bigNumber;
   cout << number;
+  int mathExpression = sum % 2;
+  cout << mathExpression << endl;
   return 0;
 }
